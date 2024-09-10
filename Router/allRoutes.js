d2afee057504f22cc9bce29854d808cf976eac8c
nafniko <naffer@hotmail.fr>
2024-09-10 14:01:56 +0200

    add pages auth

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,11 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/connexion", "connexion", "/pages/auth/connexion.html"),
+    new Route("/signup", "inscription", "/pages/auth/signup.html"),
+    new Route("/account", "mon compte", "/pages/auth/account.html"),
+    new Route("/editpassword", "mon compte", "/pages/auth/editpassword.html"),
+   
    ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
